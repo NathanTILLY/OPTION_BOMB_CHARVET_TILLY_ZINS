608ef91d6b8b08d69325ec228f00524d72bb81c2
NathanTILLY <74654828+NathanTILLY@users.noreply.github.com>
2021-04-15 09:58:37 +0200

    base killian

---
+++ constantes.py
@@ -0,0 +1,18 @@
+nombre_sprite_cote = 15
+taille_sprite = 40
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+titre_fenetre = "Bomberman"
+
+image_accueil = "images/accueil_bomberman.png"
+
+image_bloc_destructible = "images/bloc_destructible.png"
+image_fond = "images/accueil_bomberman.png"
+image_bloc_indestructible = "images/bloc_indestructible.png"
+image_bombe = "images/bomb.png"
+image_sol = "images/sol.png"
+
+p1_droite = "images/J1_rightkey.png"
+p1_gauche = "images/J1_leftkey.png"
+p1_haut = "images/J1_upkey.png"
+p1_bas = "images/J1_downkey.png"
